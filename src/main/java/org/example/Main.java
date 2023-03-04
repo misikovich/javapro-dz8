bb011c935cc6280e9f19cd63866baddabb8de3d8
misikovich <misikovich.dev@gmail.com>
2023-03-04 17:17:00 +0100

    added classes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,18 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        PalindromeParser parser = new PalindromeParser();
+
+
+        String quitCmd = "q:";
+        for (String userLine; !(userLine = sc.nextLine()).equals(quitCmd); System.out.printf("Type '%s' to exit\n", quitCmd)) {
+            boolean isPalindrome = parser.isPalindrome(userLine);
+            System.out.printf("'%s' is %sa palindrome\n",userLine , isPalindrome ? "" : "not ");
+            System.out.println(isPalindrome);
+        }
     }
 }
