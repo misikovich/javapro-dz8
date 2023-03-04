bae8acf7551e9716953a8023572710443fae4ab6
misikovich <misikovich.dev@gmail.com>
2023-03-04 17:30:32 +0100

    made PalindromeParserTest

--- src/test/java/PalindromeParserTest.java
+++ src/test/java/PalindromeParserTest.java
@@ -4,5 +4,16 @@ import org.junit.Test;
 
 public class PalindromeParserTest {
 
-    
+    @Test
+    public void isPalindromeTest(){
+        String truePalindrome = "A man, a plan, a canal: Panama";
+        String falsePalindrome = "race a car";
+        String somethingPalindrome = " ";
+
+        PalindromeParser parser = new PalindromeParser();
+
+        Assert.assertTrue(parser.isPalindrome(truePalindrome));
+        Assert.assertFalse(parser.isPalindrome(falsePalindrome));
+        Assert.assertTrue(parser.isPalindrome(somethingPalindrome));
+    }
 }
