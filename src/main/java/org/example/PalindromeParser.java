a9dc89fb785caa4d251b5658810e13784e625917
misikovich <misikovich.dev@gmail.com>
2023-03-04 17:17:29 +0100

    finished PalindromeParser

--- src/main/java/org/example/PalindromeParser.java
+++ src/main/java/org/example/PalindromeParser.java
@@ -2,5 +2,20 @@ package org.example;
 
 
 public class PalindromeParser {
+    public boolean isPalindrome(String palindrome) {
+        String palindromeNormalized = palindrome.toLowerCase().replaceAll("[^a-zA-Z0-9]", "");
 
+        int length = palindromeNormalized.length();
+        int halfLength = length / 2;
+
+        String part1 = palindromeNormalized.substring(0, halfLength);
+        String part2 = new StringBuilder(palindromeNormalized.substring(halfLength + length % 2)).reverse().toString();
+
+        if (length % 2 == 1) {
+            part1 = part1.substring(0, halfLength);
+        }
+
+        return part1.equals(part2);
+
+    }
 }
