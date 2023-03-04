3c8c50f500f2da978d6d427f2c4d7fee6e829fdd
misikovich <misikovich.dev@gmail.com>
2023-03-04 17:20:15 +0100

    finished UniqueChecker

--- src/main/java/org/example/UniqueChecker.java
+++ src/main/java/org/example/UniqueChecker.java
@@ -1,4 +1,13 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class UniqueChecker {
+    public boolean isUnique(int[] possiblyUnique) {
+        return Arrays.stream(possiblyUnique)
+                .boxed()
+                .collect(Collectors.toSet())
+                .size() < possiblyUnique.length;
+    }
 }
