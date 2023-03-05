e33655d9018a71481b1e58d17ab76dbfc91b76a6
misikovich <misikovich.dev@gmail.com>
2023-03-05 10:36:11 +0100

    SumArrayParser finished

--- src/main/java/org/example/SumArrayParser.java
+++ src/main/java/org/example/SumArrayParser.java
@@ -3,5 +3,15 @@ package org.example;
 import java.util.Objects;
 
 public class SumArrayParser {
+    public Integer[] findTwoSums(int[] nums, int target) {
 
+        for (int i = 0; i < nums.length; i++) {
+            int num1 = nums[i];
+            for (int j = 0; j < nums.length; j++) {
+                int num2 = nums[j];
+                if (Objects.equals(num1 + num2, target)) return new Integer[] {i, j};
+            }
+        }
+        return new Integer[] {};
+    }
 }
